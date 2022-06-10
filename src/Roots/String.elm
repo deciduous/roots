f03871a1b2249e48293d3aa767d3b5ecfcb038b3
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-06-09 22:08:50 -0400

    stripPrefix / stripPrefix_

---
+++ src/Roots/String.elm
@@ -0,0 +1,22 @@
+module Roots.String exposing
+    ( stripPrefix
+    , stripPrefix_
+    )
+
+
+stripPrefix : String -> String -> Maybe String
+stripPrefix prefix string =
+    if String.startsWith prefix string then
+        Just (String.dropLeft (String.length prefix) string)
+
+    else
+        Nothing
+
+
+stripPrefix_ : String -> String -> String
+stripPrefix_ prefix string =
+    if String.startsWith prefix string then
+        String.dropLeft (String.length prefix) string
+
+    else
+        string
