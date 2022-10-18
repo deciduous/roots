4dc7f58f190eccdf9cddd040513f7b61500cbe84
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-10-18 12:29:13 -0400

    ifte

--- src/Roots.elm
+++ src/Roots.elm
@@ -1,6 +1,7 @@
 module Roots exposing
     ( Array
     , Dict
+    , ifte
     , isEven
     , isOdd
     )
@@ -17,9 +18,19 @@ type alias Dict k v =
     Dict.Dict k v
 
 
+ifte : Bool -> a -> a -> a
+ifte p t f =
+    if p then
+        t
+
+    else
+        f
+
+
 isEven : Int -> Bool
 isEven n =
     modBy 2 n == 0
+
 
 isOdd : Int -> Bool
 isOdd n =
