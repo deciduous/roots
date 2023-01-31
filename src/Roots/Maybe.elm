fde87cf8d67d03e79ac993fc65ad03ddfab7975d
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-01-31 14:23:41 -0500

    add Maybe.isJust

--- src/Roots/Maybe.elm
+++ src/Roots/Maybe.elm
@@ -1,6 +1,16 @@
 module Roots.Maybe exposing (..)
 
 
+isJust : Maybe a -> Bool
+isJust x =
+    case x of
+        Nothing ->
+            False
+
+        Just _ ->
+            True
+
+
 isNothing : Maybe a -> Bool
 isNothing x =
     case x of
