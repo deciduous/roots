8f47751da38cf93787a6c06143fe0941cac33064
Mitchell Dalvi Rosen <mitchellwrosen@gmail.com>
2025-11-24 10:45:38 -0500

    html

--- src/Roots/Html.elm
+++ src/Roots/Html.elm
@@ -1,117 +1,27 @@
-module Roots.Html exposing
-    ( Html, a, div, img, input, label, none, span, text, textarea
-    , if_, when
-    , toHtml
-    )
+module Roots.Html exposing (if_, none, when)
 
-{-| Html.
-
-@docs Html, a, div, img, input, label, none, span, text, textarea
-@docs if_, when
-@docs toHtml
-
--}
-
-import Html
-import Roots.Attr exposing (Attr)
-import Roots.Internal.Attr as Attr
-
-
-type Html a
-    = Zero
-    | One (Html.Html a)
-    | Many (List (Html.Html a))
-
-
-toHtml : List (Html a) -> List (Html.Html a)
-toHtml =
-    List.foldr
-        (\html acc ->
-            case html of
-                Zero ->
-                    acc
-
-                One html1 ->
-                    html1 :: acc
-
-                Many html1 ->
-                    html1 ++ acc
-        )
-        []
-
-
-concat : List (Html a) -> Html a
-concat x =
-    case toHtml x of
-        [] ->
-            Zero
-
-        [ y ] ->
-            One y
-
-        y ->
-            Many y
-
-
-a : List (Attr a) -> List (Html a) -> Html a
-a x y =
-    One (Html.a (Attr.compile x) (toHtml y))
-
-
-img : List (Attr a) -> List (Html a) -> Html a
-img x y =
-    One (Html.img (Attr.compile x) (toHtml y))
-
-
-div : List (Attr a) -> List (Html a) -> Html a
-div x y =
-    One (Html.div (Attr.compile x) (toHtml y))
-
-
-input : List (Attr a) -> List (Html a) -> Html a
-input x y =
-    One (Html.input (Attr.compile x) (toHtml y))
-
-
-label : List (Attr a) -> List (Html a) -> Html a
-label x y =
-    One (Html.label (Attr.compile x) (toHtml y))
+import Html exposing (Html)
 
 
 none : Html a
 none =
-    Zero
+    Html.text ""
 
 
-span : List (Attr a) -> List (Html a) -> Html a
-span x y =
-    One (Html.span (Attr.compile x) (toHtml y))
-
-
-text : String -> Html a
-text s =
-    One (Html.text s)
-
-
-textarea : List (Attr a) -> Html a
-textarea x =
-    One (Html.textarea (Attr.compile x) [])
-
-
-if_ : Bool -> List (Html a) -> Html a
+if_ : Bool -> Html a -> Html a
 if_ b x =
     if b then
-        concat x
+        x
 
     else
-        Zero
+        Html.text ""
 
 
-when : Maybe a -> (a -> List (Html b)) -> Html b
+when : Maybe a -> (a -> Html b) -> Html b
 when mx f =
     case mx of
         Just x ->
-            concat (f x)
+            f x
 
         Nothing ->
-            Zero
+            Html.text ""
